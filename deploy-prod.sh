a2932983c44f5b47adedd8aedebab8695a3f95a6
Evgeny Svirsky <evgeny@cere.io>
2021-02-11 17:37:07 +0300

    * Added deployment scripts * Added ENV files

---
+++ deploy-prod.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+cp .env .env.tmp
+cp .env.stage.example .env
+
+export NODE_ENV=production&&nvm exec npm run build
+
+aws s3 sync build/ s3://block-scanner.cere.io --profile=cerebellum
+# aws cloudfront create-invalidation --distribution-id **** --paths "/*" --profile=cerebellum
+
+cp .env.tmp .env
+rm -rf .env.tmp
