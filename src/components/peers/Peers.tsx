ea881ebb8b68fb7907ac7d59c6946605e58da42f
Karthikeyan <karthikeyan@cere.io>
2021-03-11 10:39:42 +0530

    adding loader for peers dashboard

--- src/components/peers/Peers.tsx
+++ src/components/peers/Peers.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Peer } from '../../models/Peers';
 
 interface PeerProps {
@@ -16,38 +17,57 @@ const Peers = ({ items, network, isLoading, fetchPeers }: PeerProps) => {
 
   return (
     <>
-      {items.length !== 0 ? (
+      {isLoading === true ? (
         <>
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">Peer Id</th>
-                <th scope="col">Roles</th>
-                <th scope="col">IP</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items.map((item: Peer, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{item.peerId}</td>
-                    <td>{item.roles}</td>
-                    <td>{item.ip}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <span>
+            <FontAwesomeIcon
+              icon={faSpinner}
+              className="mr-2"
+              rotation={180}
+              spin
+            />
+          </span>
         </>
       ) : (
-        <div className="p-5 f40 lato-bold my-auto col-12 bg-light">
-          <div className="d-flex justify-content-center">
-            No records found
+        <div>
+          {
+            items.length !== 0 ? (
+              <>
+                <table className="table table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">Peer Id</th>
+                      <th scope="col">Roles</th>
+                      <th scope="col">IP</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {items.map((item: Peer, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{item.peerId}</td>
+                          <td>{item.roles}</td>
+                          <td>{item.ip}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </>
+            ) : (
+              <div className="p-5 f40 lato-bold my-auto col-12 bg-light">
+                <div className="d-flex justify-content-center">
+                  No records found
+          </div>
+              </div>
+            )
+          }
         </div>
-        </div>
-      )}
+      )
+      }
     </>
   )
 }
+
 
 export default Peers;
