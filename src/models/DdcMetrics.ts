07b35ea92e8846a4e3a0ef1491f5cbd11a8bad2e
Andrei Navoichyk <andrei@cere.io>
2021-12-07 23:21:52 +0300

    removed ddc metrics

--- src/models/DdcMetrics.ts
+++
@@ -1,8 +0,0 @@
-export class DdcMetrics {
-  constructor(
-    readonly nodesCount: string,
-    readonly totalPartitions: string,
-    readonly reservedPartitions: string,
-    readonly availablePartitions: string,
-  ) {}
-}
